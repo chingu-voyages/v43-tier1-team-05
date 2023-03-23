78fc0cba4795bec673199fb1ab176893389d4512
ofthewildfire <lissajace@gmail.com>
2023-03-23 20:01:14 +0200

    madlib story show/hide function

--- madlib-react/src/App.jsx
+++ madlib-react/src/App.jsx
@@ -3,11 +3,27 @@ import MadlibForm from "./components/MadlibForm";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [text, setText] = useState("[--user input--]");
+
+	function getTextInput(e) {
+		setText(e.target.value);
+	}
+
+	function getStory() {
+		setActive((prev) => !prev);
+		console.log(active);
+	}
 
 	return (
 		<div className="App">
-			<h1>Hello+App</h1>
+			<h1>Test+App</h1>
+			<p className="exclude">Testing the blur effect on story.</p>
+			<MadlibForm
+				active={active}
+				type={text}
+				getTextInput={getTextInput}
+			/>
+			<button onClick={() => getStory()}>Get story</button>
 		</div>
 	);
 }
--- madlib-react/src/components/MadlibForm.jsx
+++ madlib-react/src/components/MadlibForm.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "../App.css";
 
-const MadlibForm = () => {
-	return <div>MadlibForm</div>;
+const MadlibForm = ({ type, getTextInput, active }) => {
+	const style = {
+		color: active ? "black" : "transparent",
+	};
+
+	return (
+		<div className="story">
+			<input
+				type="text"
+				onChange={(event) => getTextInput(event)}
+			/>
+			<p style={style}>
+				Once upon a time, there was a <span>{type}</span> <span>{type}</span>{" "}
+				that lived in a <span>{type}</span> <span>{type}</span> One day, it{" "}
+				<span>{type}</span> <span>{type}</span> and became very{" "}
+				<span>{type}</span>
+			</p>
+		</div>
+	);
 };
 
 export default MadlibForm;
