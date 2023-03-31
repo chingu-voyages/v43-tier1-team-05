64f768aad3f2dd562bc4f464d6b46945008243f5
innaslota <innaslota8@gmail.com>
2023-03-31 11:15:07 +0300

    init: move project to vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from "./Header";
+import Footer from "./Footer";
+import Section from "./Section";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Section />
+      <Footer />  
+    </div>
+  )
+}
+
+export default App
