eb5241c280dbb07f0192cc94db9e9bb3541c53d5
ofthewildfire <lissajace@gmail.com>
2023-04-03 06:24:29 +0200

    deleted all data from easyStories.js file

--- EasyStories.js
+++ EasyStories.js
@@ -1,9 +1,3 @@
-const easy = [
-	{
-		difficulty: "Easy",
-		story:
-			"<span>{Name}</span> loved to <span>{verb}</span> in the <span>{adjective}</span> <span>{place}</span>. One day, they discovered a <span>{adjective} {noun}</span> that was <span>{adverb}</span> <span>{verb}</span>ing. ",
-	},
-];
+const easy = [];
 
 export default easy;
